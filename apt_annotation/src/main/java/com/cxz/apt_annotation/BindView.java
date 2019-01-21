f844a4d6de15c1dba11fe4b7d54b98f127944490
chenxz <563859095@qq.com>
2019-01-21 11:39:00 +0800

    commit

---
+++ apt_annotation/src/main/java/com/cxz/apt_annotation/BindView.java
@@ -0,0 +1,17 @@
+package com.cxz.apt_annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author admin
+ * @date 2019/1/21
+ * @desc
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.FIELD)
+public @interface BindView {
+    int value();
+}
