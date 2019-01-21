3c2cc3f662e1ebf5cd58a8b8a6471225b920edda
chenxz <563859095@qq.com>
2019-01-21 11:42:34 +0800

    commit

--- app/src/main/java/com/cxz/aptsample/MainActivity.java
+++ app/src/main/java/com/cxz/aptsample/MainActivity.java
@@ -2,12 +2,22 @@ package com.cxz.aptsample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.cxz.apt_annotation.BindView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.textView)
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        textView.setText("Test APT");
+
     }
 }
